57aaa6d90a841a8877552a7eb71c2b50ec3f2f80
Irena Zivkovic <zivkovicirena3@gmail.com>
2023-04-04 11:15:58 +0200

    Izmenjena klasa Knjiga i dodata klasa Autor

---
+++ src/biblioteka/Autor.java
@@ -0,0 +1,8 @@
+package biblioteka;
+
+public class Autor {
+
+	private String ime;
+	private String prezime;
+	
+}
--- src/biblioteka/Knjiga.java
+++ src/biblioteka/Knjiga.java
@@ -1,10 +1,12 @@
 package biblioteka;
 
+import java.util.ArrayList;
+
 public class Knjiga {
 
 	private String naziv;
 	private long isbn;
-	private String autor;
+	private ArrayList<Autor> autori;
 	private String izdavac;
 	private int izdanje;
 	
