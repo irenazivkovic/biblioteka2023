299989be132c4de1efc68384869d2aa20b85a8fd
Irena Zivkovic <zivkovicirena3@gmail.com>
2023-04-04 12:20:56 +0200

    toString i equals

--- src/biblioteka/Knjiga.java
+++ src/biblioteka/Knjiga.java
@@ -10,5 +10,54 @@ public class Knjiga {
 	private String izdavac;
 	private int izdanje;
 	
+	@Override
+	public String toString() {
+		return "Knjiga [naziv=" + naziv + ", isbn=" + isbn + ", autori=" + autori + ", izdavac=" + izdavac
+				+ ", izdanje=" + izdanje + "]";
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((autori == null) ? 0 : autori.hashCode());
+		result = prime * result + (int) (isbn ^ (isbn >>> 32));
+		result = prime * result + izdanje;
+		result = prime * result + ((izdavac == null) ? 0 : izdavac.hashCode());
+		result = prime * result + ((naziv == null) ? 0 : naziv.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Knjiga other = (Knjiga) obj;
+		if (autori == null) {
+			if (other.autori != null)
+				return false;
+		} else if (!autori.equals(other.autori))
+			return false;
+		if (isbn != other.isbn)
+			return false;
+		if (izdanje != other.izdanje)
+			return false;
+		if (izdavac == null) {
+			if (other.izdavac != null)
+				return false;
+		} else if (!izdavac.equals(other.izdavac))
+			return false;
+		if (naziv == null) {
+			if (other.naziv != null)
+				return false;
+		} else if (!naziv.equals(other.naziv))
+			return false;
+		return true;
+	}
+	
 	
 }
