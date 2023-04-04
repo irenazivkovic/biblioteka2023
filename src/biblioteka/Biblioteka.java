f34dbc964b569e4f65e8ee883eeb44add0d763a1
Irena Zivkovic <zivkovicirena3@gmail.com>
2023-04-04 12:41:55 +0200

    Logicka kontrola

---
+++ src/biblioteka/Biblioteka.java
@@ -0,0 +1,35 @@
+package biblioteka;
+
+import java.util.List;
+
+import biblioteka.interfejs.BibliotekaInterfejs;
+
+public class Biblioteka implements BibliotekaInterfejs{
+
+	@Override
+	public void dodajKnjigu(Knjiga knjiga) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void obrisiKnjigu(Knjiga knjiga) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public List<Knjiga> VratiSveKnjige() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Knjiga> pronadjiKnjigu(Autor autor, long ISBN, String naslov, String izdavac) {
+		if(autor==null && ISBN<=0 && naslov==null && izdavac==null)
+			throw new IllegalArgumentException("Morate ineti bar neki kriterijum za pretragu");
+		
+		return null;
+	}
+
+}
