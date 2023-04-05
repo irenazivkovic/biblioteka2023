8e35d6ef3089890eb1ead91bbca6c096ec9227de
Irena Zivkovic <zivkovicirena3@gmail.com>
2023-04-05 09:31:57 +0200

    Get i Set metode klasa Autor i Knjiga

--- src/biblioteka/Autor.java
+++ src/biblioteka/Autor.java
@@ -4,6 +4,42 @@ public class Autor {
 
 	private String ime;
 	private String prezime;
+	
+	public Autor() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	public Autor(String ime, String prezime) {
+		super();
+		this.ime = ime;
+		this.prezime = prezime;
+	}
+
+	public String getIme() {
+		return ime;
+	}
+	public void setIme(String ime) {
+		if(ime==null)
+			throw new NullPointerException("Ime ne sme da bude null!");
+		
+		if(ime.isEmpty())
+			throw new IllegalArgumentException("Ime ne sme da bude prazno!");
+		
+		this.ime = ime;
+	}
+	public String getPrezime() {
+		return prezime;
+	}
+	public void setPrezime(String prezime) {
+		if(prezime==null)
+			throw new NullPointerException("Prezime ne sme da bude null!");
+		
+		if(prezime.isEmpty())
+			throw new IllegalArgumentException("Prezime ne sme da bude prazno!");
+		
+		this.prezime = prezime;
+	}
+	
 	@Override
 	public int hashCode() {
 		final int prime = 31;
--- src/biblioteka/Knjiga.java
+++ src/biblioteka/Knjiga.java
@@ -1,15 +1,73 @@
 package biblioteka;
 
-import java.util.ArrayList;
+import java.util.List;
 
 public class Knjiga {
 
 	private String naziv;
 	private long isbn;
-	private ArrayList<Autor> autori;
+	private List<Autor> autori;
 	private String izdavac;
 	private int izdanje;
 	
+	public String getNaziv() {
+		return naziv;
+	}
+
+	public void setNaziv(String naziv) {
+		if(naziv==null)
+			throw new NullPointerException("Naziv ne sme da bude null!");
+		
+		if(naziv.isEmpty())
+			throw new IllegalArgumentException("Naziv ne sme da bude prazan!");
+		
+		this.naziv = naziv;
+	}
+
+	public long getIsbn() {
+		return isbn;
+	}
+
+	public void setIsbn(long isbn) {
+		if (isbn <= 0)
+			throw new IllegalArgumentException("ISBN ne sme biti nula niti manji");
+		
+		this.isbn = isbn;
+	}
+
+	public List<Autor> getAutori() {
+		return autori;
+	}
+
+	public void setAutori(List<Autor> autori) {
+		this.autori = autori;
+	}
+
+	public String getIzdavac() {
+		return izdavac;
+	}
+
+	public void setIzdavac(String izdavac) {
+		if (izdavac == null)
+			throw new NullPointerException("Izdavac ne sme biti null");
+		
+		if (izdavac.isEmpty())
+			throw new IllegalArgumentException("Izdavac ne sme biti prazan");		
+		
+		this.izdavac = izdavac;
+	}
+
+	public int getIzdanje() {
+		return izdanje;
+	}
+
+	public void setIzdanje(int izdanje) {
+		if (izdanje < 1)
+			throw new IllegalArgumentException("Izdanje mora biti 1 ili vece");
+		
+		this.izdanje = izdanje;
+	}
+
 	@Override
 	public String toString() {
 		return "Knjiga [naziv=" + naziv + ", isbn=" + isbn + ", autori=" + autori + ", izdavac=" + izdavac
